c06eb454a896d8b1de457e0ed12f551a0f9f8d45
Prostoyjitel <xelzim.rubina.2021@gmail.com>
2023-04-16 18:06:03 +0400

    feat: creat register useForm half

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,8 +1,41 @@
 import style from "./registration.module.css"
 import Rbook from "../images/Rbook.png"
 import google from "../images/google.png"
+import { useForm } from "react-hook-form"
+import { errorMes, minMaxLengtMes, requeridMes } from "../../constants/errorMessage"
+import { useRef } from "react"
 
 export function Registration() {
+
+    const {
+        register,
+        formState: {
+            errors
+        },
+        handleSubmit,
+        watch,
+        setError,
+        clearErrors,
+        reset
+    } = useForm()
+
+
+    const password = watch("password", "");
+    const repeatPassword = watch("repeatPassword", "");
+
+
+    const pass1Ref = useRef(null)
+    const pass2Ref = useRef(null)
+
+    const onSubmit = data => {
+        // reset() 
+        if (password === repeatPassword) {
+            console.log(JSON.stringify(data));
+        }
+        else {
+            console.log(false);
+        }
+    }
 
 
     return (
@@ -11,23 +44,117 @@ export function Registration() {
                 <h2 className={style.regheading}> This is  Register page </h2>
                 <img className={style.imgbook} src={Rbook} alt="" />
             </div>
-            <form action="" >
+            <form onSubmit={handleSubmit(onSubmit)} >
 
                 <button className={style.googleBtn}><img src={google} alt="" className={style.googleImg} /> Sing up with Google</button>
 
                 <div className={style.border}>
                     <div className={style.spBor}></div>
-                        <span>or</span>
+                    <span>or</span>
                     <div className={style.spBor}></div>
                 </div>
 
-                <input type="text" placeholder="First name" />
-                <input type="text" placeholder="Last Name" />
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="password" placeholder="Password" />
-                <button className={style.but} type="submit">Submit</button>|
-            </form>
-        </div>
+                <input
+                    {...register("firstName", {
+                        required: requeridMes.reqMes,
+                        minLength: {
+                            value: 3,
+                            message: minMaxLengtMes.minName
+                        },
+                        maxLength: {
+                            value: 10,
+                            message: minMaxLengtMes.maxName
+                        }
+                    }
+                    )}
+
+
+                    type="text" placeholder="First name" />
+
+                <div>
+                    {errors?.firstName && <p style={{ color: "red" }}>{errors?.firstName?.message || "Error!"}</p>}
+                </div>
+
+                <input
+                    {...register("lastName", {
+                        required: requeridMes.reqMes,
+                        minLength: {
+                            value: 3,
+                            message: minMaxLengtMes.minName
+                        },
+                        maxLength: {
+                            value: 10,
+                            message: minMaxLengtMes.maxName
+                        }
+                    }
+                    )}
+
+
+                    type="text" placeholder="Last Name" />
+                <div>
+                    {errors?.lastName && <p style={{ color: "red" }}>{errors?.lastName?.message || "Error!"}</p>}
+                </div>
+                <input
+
+                    {
+                    ...register("email", {
+                        required: requeridMes.reqMes,
+                        pattern: {
+                            value: /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                            message: errorMes.Email
+                        }
+
+
+                    })
+                    }
+                    type="email" placeholder="Email" />
+                <div>
+                    {errors?.email && <p style={{ color: "red" }}>{errors?.email?.message || "Error!"}</p>}
+                </div>
+                <input
+                    ref={pass1Ref}
+                    {
+                    ...register("password", {
+                        required: requeridMes.reqMes,
+                        pattern: {
+                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                            message: errorMes.Password
+                        }
+                    })
+                    }
+                    type="password" placeholder="Password"
+
+                />
+                <div>
+                    {errors?.password && <p style={{ color: "red" }}>{errors?.password?.message || "Error!"}</p>}
+                </div>
+
+                <input
+                    ref={pass2Ref}
+                    {
+                    ...register("repeatPassword", {
+                        required: requeridMes.reqMes,
+                        pattern: {
+                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                            message: errorMes.Password
+                        },
+                        // validate: value => value === pass1Ref.current?.value || "Error"
+
+
+                    })
+                    }
+                    type="password" placeholder="Repeat password"
+
+                />
+                <div>
+                    {errors?.repeatPassword && <p style={{ color: "red" }}>{errors?.repeatPassword?.errorMes.passReap}</p>}
+                </div>
+
+
+                <button
+                    className={style.but} type="submit">Submit</button>
+            </form >
+        </div >
+
     )
 }
