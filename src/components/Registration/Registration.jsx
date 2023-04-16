26961a38f11e8c5a289811c3bcc6cfa4e89a5578
KarenPalyann <karenpalyan777@gmail.com>
2023-04-16 15:06:11 +0400

    feact: button

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,5 +1,6 @@
 import style from "./registration.module.css"
 import Rbook from "../images/Rbook.png"
+import google from "../images/google.png"
 
 export function Registration() {
 
@@ -10,7 +11,17 @@ export function Registration() {
                 <img src={Rbook} alt="" />
             </div>
             <form action="" >
-                <button></button>
+                <button className={style.googleBtn}><img src={google} alt="" className={style.googleImg} /> Sing up with Google</button>
+
+                <div className="border">
+                    <div className={style.spBor}></div>
+                        <span>or</span>
+                    <div className={style.spBor}></div>
+                </div>
+
+                    
+                
+
                 <input type="text" placeholder="Name" />
                 <input type="text" placeholder="SurName" />
                 <input type="email" placeholder="Email" />
