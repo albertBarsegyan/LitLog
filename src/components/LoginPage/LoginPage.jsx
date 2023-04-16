e4654f3872161b3fbae62ca2ec58cd8b410a85a8
Aghas2002 <@mail.ru>
2023-04-16 16:12:19 +0400

    css change

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,23 +2,19 @@ import loginUser from "./LoginPageImages/loginUser.png"
 import style from "./LoginPage.module.css"
 function LoginPage(params) {
     return (
-        <div className={style.form}>
-            <div className={style.userImg}>
-                <img src={loginUser} alt="loginUser" />
-            </div>
-            <form action="">
-                <input type="email" className={style.formInput} placeholder="Email" /><br />
-                <input type="password" className={style.formInput} placeholder="Password" />
-                <div className={style.remember}>
-                    <label htmlFor="remember_me" className={style.remember}>
-                        <input type="checkbox" id="remember_me" />
-                        Remember me
-                    </label>
-                    <a href="#">Forget password?</a>
+        <div className={style.main}>
+            <div className={style.form}>
+                <div className={style.userImg}>
+                    <img src={loginUser} alt="loginUser" />
                 </div>
-                <button type="submit">Login</button>
-            </form>
-            <span>Not registred?<a href="#">Create an account!</a></span>
+                <form action="">
+                    <input type="email" className={style.formInput} placeholder="Email" /><br />
+                    <input type="password" className={style.formInput} placeholder="Password" />
+                    <button type="submit" className={style.loginButton}>Login</button>
+                </form>
+                <span className={style.span}>Not registred?</span>
+                <a href="#" className={style.link}>Create an account!</a>
+            </div>
         </div>
     )
 }
