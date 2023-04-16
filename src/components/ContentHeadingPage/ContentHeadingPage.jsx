6a479d4ec11cd6c13d2ceef4ee9f0b7d5016818e
2001arm <arman_stepanyan_01@bk.ru>
2023-04-16 12:54:30 +0400

    feat: creat first page

---
+++ src/components/ContentHeadingPage/ContentHeadingPage.jsx
@@ -0,0 +1,25 @@
+import style from "./headingPage.module.css"
+import backImg from "../FirstPage/images/bkimg.jpg"
+
+function HeadingPage() {
+    return (
+        <div className={style.wrapper}>
+            <div className={style.imgBack}></div>
+            <div className={style.content}>
+                <h1>Lit<span>Log</span></h1>
+                <p>
+                    "Are you dreaming of reading books but don't want <br />
+                    to spend time going to the library or waiting for delivery? <br />
+                    Then our website is for you! Here you will find a wide selection <br />
+                    of literature - from novels to scientific works. Our books are  <br />
+                    available online, and you can start reading right away.  <br />
+                    Our library is constantly updated with new books,  <br />
+                    so you will always find something interesting for yourself.  <br />
+                    Join our online library today!"
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default HeadingPage;
---
+++ src/components/FirstPage/FirstPage.jsx
@@ -0,0 +1,16 @@
+import Header from '../Header/Header'
+import HeadingPage from "../ContentHeadingPage/ContentHeadingPage";
+import Footer from '../Footer/Footer';
+
+export function FirstPage() {
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <HeadingPage />
+            </div>
+            <Footer/>
+        </div>
+    )
+}
