e55c12dc55c3796c8c1d7d49e920ec0b5968a01e
2001arm <arman_stepanyan_01@bk.ru>
2023-04-17 19:26:19 +0400

    repeat registr componet

--- src/components/Registration/RegistrationForm/RegistrationForm.jsx
+++ src/components/Registration/RegistrationForm/RegistrationForm.jsx
@@ -35,109 +35,109 @@ const RegistrationForm = () => {
     return (
         <form onSubmit={handleSubmit(onSubmit)} >
 
-                <button className={style.googleBtn}><img src={google} alt="" className={style.googleImg} /> Sing up with Google</button>
+            <button className={style.googleBtn}><img src={google} alt="" className={style.googleImg} /> Sing up with Google</button>
 
-                <div className={style.border}>
-                    <div className={style.spBor}></div>
-                    <span>or</span>
-                    <div className={style.spBor}></div>
-                </div>
+            <div className={style.border}>
+                <div className={style.spBor}></div>
+                <span>or</span>
+                <div className={style.spBor}></div>
+            </div>
 
-                <input
-                    {...register("firstName", {
-                        required: requeridMes.reqMes,
-                        minLength: {
-                            value: 3,
-                            message: minMaxLengtMes.minName
-                        },
-                        maxLength: {
-                            value: 10,
-                            message: minMaxLengtMes.maxName
-                        }
+            <input
+                {...register("firstName", {
+                    required: requeridMes.reqMes,
+                    minLength: {
+                        value: 3,
+                        message: minMaxLengtMes.minName
+                    },
+                    maxLength: {
+                        value: 10,
+                        message: minMaxLengtMes.maxName
                     }
-                    )}
+                }
+                )}
 
 
-                    type="text" placeholder="First name" />
+                type="text" placeholder="First name" />
 
-                <div>
-                    {errors?.firstName && <p style={{ color: "red" }}>{errors?.firstName?.message || "Error!"}</p>}
-                </div>
+            <div>
+                {errors?.firstName && <p style={{ color: "red" }}>{errors?.firstName?.message || "Error!"}</p>}
+            </div>
 
-                <input
-                    {...register("lastName", {
-                        required: requeridMes.reqMes,
-                        minLength: {
-                            value: 3,
-                            message: minMaxLengtMes.minName
-                        },
-                        maxLength: {
-                            value: 10,
-                            message: minMaxLengtMes.maxName
-                        }
+            <input
+                {...register("lastName", {
+                    required: requeridMes.reqMes,
+                    minLength: {
+                        value: 3,
+                        message: minMaxLengtMes.minName
+                    },
+                    maxLength: {
+                        value: 10,
+                        message: minMaxLengtMes.maxName
                     }
-                    )}
+                }
+                )}
 
 
-                    type="text" placeholder="Last Name" />
-                <div>
-                    {errors?.lastName && <p style={{ color: "red" }}>{errors?.lastName?.message || "Error!"}</p>}
-                </div>
-                <input
+                type="text" placeholder="Last Name" />
+            <div>
+                {errors?.lastName && <p style={{ color: "red" }}>{errors?.lastName?.message || "Error!"}</p>}
+            </div>
+            <input
 
-                    {
-                    ...register("email", {
-                        required: requeridMes.reqMes,
-                        pattern: {
-                            value: /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
-                            message: errorMes.Email
-                        }
-
-
-                    })
+                {
+                ...register("email", {
+                    required: requeridMes.reqMes,
+                    pattern: {
+                        value: /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+                        message: errorMes.Email
                     }
-                    type="email" placeholder="Email" />
-                <div>
-                    {errors?.email && <p style={{ color: "red" }}>{errors?.email?.message || "Error!"}</p>}
-                </div>
-                <input
-                    {
-                    ...register("password", {
-                        required: requeridMes.reqMes,
-                        pattern: {
-                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                            message: errorMes.Password
-                        }
-                    })
+
+
+                })
+                }
+                type="email" placeholder="Email" />
+            <div>
+                {errors?.email && <p style={{ color: "red" }}>{errors?.email?.message || "Error!"}</p>}
+            </div>
+            <input
+                {
+                ...register("password", {
+                    required: requeridMes.reqMes,
+                    pattern: {
+                        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                        message: errorMes.Password
                     }
-                    type="password" placeholder="Password"
+                })
+                }
+                type="password" placeholder="Password"
 
-                />
-                <div>
-                    {errors?.password && <p style={{ color: "red" }}>{errors?.password?.message || "Error!"}</p>}
-                </div>
+            />
+            <div>
+                {errors?.password && <p style={{ color: "red" }}>{errors?.password?.message || "Error!"}</p>}
+            </div>
 
-                <input
-                    {
-                    ...register("repeatPassword", {
-                        required: requeridMes.reqMes,
-                        pattern: {
-                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                            message: errorMes.Password
-                        }
-
-
-                    })
+            <input
+                {
+                ...register("repeatPassword", {
+                    required: requeridMes.reqMes,
+                    pattern: {
+                        value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                        message: errorMes.Password
                     }
-                    type="password" placeholder="Repeat password"
-                />
-                <div>
-                    {errors?.password && <p style={{ color: "red" }}>{errors?.password?.message || "Error!"}</p>}
-                </div>
-                {password !== repeatPassword && <p style={{ color: "red" }}>{errorMes.passReap}</p>}
-                <button
-                    className={style.but} type="submit">Submit</button>
-            </form >
+
+
+                })
+                }
+                type="password" placeholder="Repeat password"
+            />
+            <div>
+                {errors?.password && <p style={{ color: "red" }}>{errors?.password?.message || "Error!"}</p>}
+            </div>
+            {password !== repeatPassword && <p style={{ color: "red" }}>{errorMes.passReap}</p>}
+            <button
+                className={style.but} type="submit">Submit</button>
+        </form >
     );
 };
 
