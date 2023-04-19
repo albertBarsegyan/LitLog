e7329425b18582cf0bc470f3261d43b578f083af
Aghas2002 <@mail.ru>
2023-04-19 20:20:48 +0400

    changed

--- src/components/LoginPage/Header/Navigation/ReginBut.jsx
+++ src/components/LoginPage/Header/Navigation/ReginBut.jsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import style from "./reginBut.module.css"
-
+import { RouterConstant } from "../../../../constants/RouterConstant"
 const ReginBut = () => {
 
     const navigation = useNavigate()
 
-    return <button className={style.regin}>Sing Up <i class="fa-solid fa-user-plus"></i></button>
+    return <button className={style.regin} onClick={() => navigation(RouterConstant.RegPage)} >Sing Up <i class="fa-solid fa-user-plus"></i></button>
 };
 
 export default ReginBut;
