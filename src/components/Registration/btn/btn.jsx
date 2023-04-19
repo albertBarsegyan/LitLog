f465a3a14997af00dc6f3c0ef871ab57cedc1d5d
2001arm <arman_stepanyan_01@bk.ru>
2023-04-19 20:19:46 +0400

    fix: bug

--- src/components/Registration/btn/btn.jsx
+++ src/components/Registration/btn/btn.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 import style from './btn.module.css'
+import { RouterConstant } from '../../../constants/RoutConstant';
 
 const Login = () => {
     const navigation = useNavigate()
+    
     return (
-        <button className={style.button}> Sign In <i class="fa-solid fa-user"></i></button>
+        <button onClick={()=>navigation(RouterConstant.LoginPage)} className={style.button}> Sign In <i class="fa-solid fa-user"></i></button>
     );
 };
 
