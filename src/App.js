de5e31a84e15bd2ab4d7ba1b2b17c30f62bde19c
Prostoyjitel <xelzim.rubina.2021@gmail.com>
2023-04-27 12:40:13 +0400

    ff

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       {/* <LoginPage /> */}
       {/* <Registration /> */}
       {/* <Icons singOut={Icon.singOut} /> */}
-    <LeftSide/>
+      <LeftSide />
     </div>
   )
 }
