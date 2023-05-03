f1b11d6a91d69a006ae160a11491d5c5f8755634
Prostoyjitel <xelzim.rubina.2021@gmail.com>
2023-05-03 19:40:57 +0400

    g

--- src/components/Button/singOutBut.jsx
+++ src/components/Button/singOutBut.jsx
@@ -1,17 +1,16 @@
 import { Link, Route, Routes } from "react-router-dom";
 import style from "./button.module.css"
-import LoginPage from "../LoginPage/LoginPage";
 import { RouteConstant } from "../../constants/RouteCostant";
 
-const SingOutBut = () => {
+const SignOutBut = () => {
     return (
         <>
             <Link to={RouteConstant.LoginPage} className={style.outBtn}>
                 Sign Out
             </Link>
-           
+
         </>
     );
 };
 
-export default SingOutBut;
+export default SignOutBut;
