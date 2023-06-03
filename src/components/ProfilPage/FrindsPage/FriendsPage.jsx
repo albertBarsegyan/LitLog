a7bca1dcb58edd2a30028b419e43418be426e54e
Aghas2002 <@mail.ru>
2023-06-03 20:58:53 +0400

    edit freand page

--- src/components/ProfilPage/FrindsPage/FriendsPage.jsx
+++ src/components/ProfilPage/FrindsPage/FriendsPage.jsx
@@ -1,7 +1,20 @@
+import CenterSite from '../CenterSite/CenterSite'
+import LeftSide from '../LeftSide/LeftSide'
+import RightSide from '../RightSide/RightSide'
 import style from './friends.module.css'
 
 const FriendsPage = () => {
-  return <div>jhmgfdghjhgf</div>
+  return (
+    <div className={style.mainDiv}>
+      <div className={style.bar}>
+        <LeftSide />
+      </div>
+      <div className={style.bar}>
+        <RightSide />
+      </div>
+      jhmgfdghjhgf
+    </div>
+  )
 }
 
 export default FriendsPage
